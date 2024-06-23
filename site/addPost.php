d2383b7eb8513dbfb0a5242d4ec492f05c8d3e56
waimea-cjmooney <cjmooney@waimea.school.nz>
2024-06-24 10:55:33 +1200

    new stuffs Adding still doesnt work

---
+++ site/addPost.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'lib/utils.php';
+
+//----------------------------------------------------------------------------
+
+consoleLog($_POST, 'POST');
+consoleLog($_FILES, 'FILES');
+
+if(empty($_POST) && empty($_FILES)) die ('There was a problem uploading the file (probably too large)');
+
+//----------------------------------------------------------------------------
+// Get image data and type of uploaded file from the $_FILES super-global
+
+[
+    'data' => $imageData,
+    'type' => $imageType
+] = uploadedImageData($_FILES['image']);
+
+//----------------------------------------------------------------------------
+// Get other data from form via the $_POST super-global.
+
+$name = $_POST['title'];
+$description = $_POST['descript'];
+
+
+//----------------------------------------------------------------------------
+// Insert the thing data and image into the database
+
+$db = connectToDB();
+
+$query = 'INSERT INTO Uploads
+            (title, descript, image_type, image_data uploaded) 
+            VALUES (?, ?, ?, ?, ?)';
+
+try {
+    $stmt = $db->prepare($query);
+    $stmt->execute([$name, $description, $imageType, $imageData, date("Y/m/d")]);
+}
+catch (PDOException $e) {
+    consoleLog($e->getMessage(), 'DB Upload Picture', ERROR);
+    die('There was an error adding picture to the database');
+}
+
+//----------------------------------------------------------------------------
+// Back to see the new thing
+
+header("Location: index.php");
---
+++ site/formPost.php
@@ -0,0 +1,32 @@
+<?php 
+require '_functions.php';
+include 'partials/top.php'; 
+?>
+
+
+<h2>New Post</h2>
+
+<form method = 'post' action = 'addPost.php' enctype="multipart/form-data">
+
+       <label>Title</label>
+       <input name = 'title'
+              type = 'text' 
+              placeholder = 'e.g. My cat' 
+              required>
+
+       <label>Description</label>
+       <textarea name = 'descript'  
+              required></textarea>
+
+       <input type = "file" 
+              name = "image" 
+              accept = "image/*" 
+              required>
+
+       <input type="submit" 
+              value="Post">
+
+
+<?php
+include 'partials/bottom.php';
+?>
